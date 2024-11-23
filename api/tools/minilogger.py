4eec671bff7aff8b5c9396a5c469779f96b3c5d0
Sneaxiii <misterbalise2@gmail.com>
2024-11-23 01:40:14 +0100

    Add method for log get and delete

--- api/tools/minilogger.py
+++ api/tools/minilogger.py
@@ -1,12 +1,28 @@
+from datetime import datetime
+
+
 class MiniLog:
     def __init__(self):
-        self.log("___________________\nSession initialized")
+        self.log("___________________\nSession initialized", display_date=False)
 
     @staticmethod
-    def log(message):
+    def log(message, display_date=True):
+        print(message)
+        date = ""
+        if display_date:
+            date = f"{datetime.now()} ---- "
         with open("logged", "a") as f:
-            f.write(f"{message}\n")
-            print(message)
+            f.write(f"{date}{message}\n")
+
+    @staticmethod
+    def clear_log():
+        with open("logged", "w") as f:
+            return
+
+    @staticmethod
+    def get_log():
+        with open("logged") as f:
+            return f.read()
 
 
 ml = MiniLog()
