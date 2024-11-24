7d8e62c8dfb2c325f4bedf78fa49393a30bf89e2
Sneaxiii <misterbalise2@gmail.com>
2024-11-25 00:31:02 +0100

    Moove log path

--- api/tools/minilogger.py
+++ api/tools/minilogger.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-
+LOG_PATH = "./logs/logs"
 
 class MiniLog:
     def __init__(self):
@@ -11,17 +11,17 @@ class MiniLog:
         date = ""
         if display_date:
             date = f"{datetime.now()} ---- "
-        with open("logged", "a") as f:
+        with open(LOG_PATH, "a") as f:
             f.write(f"{date}{message}\n")
 
     @staticmethod
     def clear_log():
-        with open("logged", "w") as f:
+        with open(LOG_PATH, "w") as f:
             return
 
     @staticmethod
     def get_log():
-        with open("logged") as f:
+        with open(LOG_PATH) as f:
             return f.readlines()
 
 
